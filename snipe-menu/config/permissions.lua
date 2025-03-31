e09c3d0717d5e04fb371096b8f61ebfd7e1c66d2
itzSparkeFam <108972258+itzSparkeFam@users.noreply.github.com>
2025-03-30 21:58:35 -0500

    Fixed paintball

---
+++ snipe-menu/config/permissions.lua
@@ -0,0 +1,15 @@
+Config.GodRoles = {
+    ["god"] = "Management Staff",
+    ["dev"] = "Developer",
+    ["tdev"] = "Trial Developer",
+    ["admin"] = "Staff",
+    ["mod"] = "Trial Staff",
+}
+
+Config.Permissions = {
+    -- Sparke
+    ["license:bb75b6a1bdee8a6aba1eb930640ad145ac8e8852"] = "god",
+    ["fivem:6945251"] = "god",
+    ["discord:338562817684799489"] = "god",
+}
+
